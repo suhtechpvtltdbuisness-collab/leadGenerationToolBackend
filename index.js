6abbfdc17110eb707c8f1ced28521eed1be66ed0
Yuvraj singh <yuvrajsingh@Yuvrajs-MacBook-Air.local>
2026-02-13 16:20:30 +0530

    sdf

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
+app.get('/health', (req, res) => {
     res.send('hii from lead generation backend ');
 })
 
